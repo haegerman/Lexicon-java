75ab219a47a2e34f7e2e5b07cf6485d234bd9cf9
Oliver <oadfeldtstill@gmail.com>
2018-03-06 14:20:53 +0100

    fist commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>(); 
+		
+		list.add("Bertil"); list.add("Anna"); list.add("Lars"); 
+		
+		System.out.println(list.get(1)); 
+		System.out.println(list.size()); 
+		System.out.println(list.contains("Ulf")); 
+		System.out.println(list.remove(0));
+		list.add("Oliver"); 
+		System.out.println(list.toString());
+		
+		
+		LinkedList<String> list2 = new LinkedList<>(); 
+		
+		list2.add("Bertil"); list2.add("Anna"); list2.add("Lars"); 
+		
+		System.out.println(list2.get(1)); 
+		System.out.println(list2.size()); 
+		System.out.println(list2.contains("Ulf")); 
+		System.out.println(list2.remove(0)); 
+		System.out.println(list2.toString());
+
+		
+		List<String> list3 = new LinkedList<>(); 
+		
+		list3.add("Bertil"); list3.add("Anna"); list3.add("Lars"); 
+		
+		System.out.println(list3.get(1)); 
+		System.out.println(list3.size()); 
+		System.out.println(list3.contains("Ulf")); 
+		System.out.println(list3.remove(0)); 
+		System.out.println(list.toString());
+	}
+
+}
